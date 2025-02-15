e6108d58f1821ba59e1b6650aaf8600d4d7e0f6c
Jakub Kozłowski <kubukoz@gmail.com>
2025-02-15 18:50:14 +0100

    Add vite-plugin-scalajs

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
+  plugins: [scalaJSPlugin({ cwd: "../", projectID: "web" })],
   server: {
     proxy: {
       // For requests to /api/**, pass to backend
